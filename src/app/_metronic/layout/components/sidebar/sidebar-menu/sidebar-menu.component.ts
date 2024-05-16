2695e1b0ed479ced16e582891271bb67cd4af930
majd2607 <majd.betabessi@esprit.tn>
2024-05-16 14:12:56 +0100

    alco integrated

--- src/app/_metronic/layout/components/sidebar/sidebar-menu/sidebar-menu.component.ts
+++ src/app/_metronic/layout/components/sidebar/sidebar-menu/sidebar-menu.component.ts
@@ -7,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarMenuComponent implements OnInit {
 
-  constructor() { }
+  userRole: string;
 
-  ngOnInit(): void {
-  }
+  constructor() { }
+  async ngOnInit() {
+      // Retrieve userData from local storage
+      const userData = localStorage.getItem('userData');
+      console.log(userData);
+      
+      if (userData) {
+        const user = JSON.parse(userData);
+        this.userRole = user.role; // Assuming 'role' is the attribute containing the user's role
+        console.log('role', this.userRole);
+        
+      }
+    }
 }
